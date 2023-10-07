35e289600867d1f530a831803b26e946b6a4e069
Dakota Vikdal <dakota.james19@hotmail.com>
2023-10-07 18:52:16 -0400

    Finishing touches on password checker

--- check_my_password.py
+++ check_my_password.py
@@ -1,7 +1,8 @@
 import requests
 import hashlib
+import sys
 
-# request the status code of a hashed password.
+# Request the status code of a hashed password.
 def request_api_data(hash_char):
     url = 'https://api.pwnedpasswords.com/range/' + hash_char
     res = requests.get(url)
@@ -9,11 +10,30 @@ def request_api_data(hash_char):
         raise RuntimeError(f'Error fetching {res.status_code}, try again bucko!')
     return res
 
+# Check to see how many times a password has been leaked.
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
 # Generate a SHA-1 hash from the provided password for security checks.   
 def pwned_api_check(password):
-    print(hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
+    hashed_pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = hashed_pass[:5], hashed_pass[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
 
+# Print how many times a password has been leaked, if any.
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times. You should consider changing your password.')
+        else:
+            print('Your password is really great! It wasn\'t found a single time. Nice choice!')
+    return 'done!'
 
-print(request_api_data('C8AFC'))
-pwned_api_check('123')
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
