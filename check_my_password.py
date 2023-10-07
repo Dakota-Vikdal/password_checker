7b6c53a99416c8b6e59a5267a914934112b7caef
Dakota Vikdal <dakota.james19@hotmail.com>
2023-10-07 17:48:42 -0400

    built status code request function. Began a hash function to check if a password is secure.

--- check_my_password.py
+++ check_my_password.py
@@ -1,7 +1,19 @@
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + 'C8AFC'
-res = requests.get(url)
+# request the status code of a hashed password.
+def request_api_data(hash_char):
+    url = 'https://api.pwnedpasswords.com/range/' + hash_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching {res.status_code}, try again bucko!')
+    return res
 
-print(res)
-print(url)
+
+# Generate a SHA-1 hash from the provided password for security checks.   
+def pwned_api_check(password):
+    print(hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
+
+
+print(request_api_data('C8AFC'))
+pwned_api_check('123')
